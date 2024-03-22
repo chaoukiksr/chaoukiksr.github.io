7a512d8f4ccc3f03323ef32b582dc1ea74935941
chaoukiksr <chaouki.kessouri@se.univ-bejaia.dz>
2024-03-22 10:53:08 +0000

    updating syntaxe, documentation and modularity.
    Introducing arrow functions and ternary operators, defining proper CSS file and adding more explicative comments.

--- automat.js
+++ automat.js
@@ -1,4 +1,6 @@
-// appler la faunction automate a chaque click d'une touche de clavier
+// Ce programme verifier l'appartenance des mot 
+// fournis par l'utilisateur à un langage definie par :c(b|c)*
+// il s'agit d'un automate à état finis detirminist
 let input = document.getElementById("input");
 let reponse = document.getElementById("reponse");
 let body = document.body;
@@ -11,97 +13,32 @@ input.addEventListener('keyup',()=>{
    let tableau = mot.split('');
    let i = 0; //indice de premier caractere dans le tableau
 
-
-   // function de depart
-   // function verifier(tableau) {
-   //    if (tableau[0] == 'c') {
-   //       return Q1(tableau, i + 1)
-   //    }
-   //    if (tableau[0] == 'b') {
-   //       return Q2(tableau, i + 1)
-   //    }
-   //    return 0
-   // }
-
-   // version 2.0
-   
    // etat Q1
-   // function Q1(tableau, i) {
-   //    if (tableau[i] == 'c') {
-   //       return Q1(tableau, i + 1);
-   //    }
-   //    if (tableau[i] == 'b') {
-   //       return Q3(tableau, i + 1);
-   //    }
-   //    return 0;
-   // }
 
-   // v 2.0
-   Q1 = (tableau, i) =>{
+   const Q1 = (tableau, i) =>{
       return (tableau[i] == 'c') ? Q1(tableau, i + 1) : (tableau[i] == 'b') ? Q3(tableau, i + 1) : 0;
    }
    // etat Q2
-   // function Q2(tableau, i) {
-   //    if (tableau[i] == 'c') {
-   //       return Q2(tableau, i + 1);
-   //    }
-   //    if (tableau[i] == 'b') {
-   //       return Q4(tableau, i + 1);
-   //    }
-   //    return 0;
-   // }
 
-   Q2 = (tableau, i) => {
+   const Q2 = (tableau, i) => {
       return (tableau[i] == 'c') ? Q2(tableau, i + 1) : (tableau[i] == 'b') ? Q4(tableau, i + 1) : 0;
    }
    // etat Q3
-   // function Q3(tableau, i) {
-   //    if (tableau[i] == 'c') {
-   //       return Q3(tableau, i + 1);
-   //    }
-   //    if (tableau[i] == 'b') {
-   //       return Q5(tableau, i + 1);
-   //    }
-   //    return 0;
-   // }
-   Q3 = (tableau, i) => {
+
+   const Q3 = (tableau, i) => {
       return (tableau[i] == 'c') ? Q3(tableau, i + 1) : (tableau[i] == 'b') ? Q5(tableau, i + 1) : 0;
    }
    // etat Q4 (etat final)
-   // function Q4(tableau, i) {
-   //    if (tableau[i] == 'c') {
-   //       return Q4(tableau, i + 1);
-   //    }
-   //    if (tableau[i] == 'b') {
-   //       return Q1(tableau, i + 1);
-   //    }
-   //    //    s'il s'agit de dernier caractere de mot
-   //    if (tableau[i] == ' ') {
-   //       return 1;
-   //    }
-   //    return 0
-   // }
-   Q4 = (tableau, i) => {
+
+   const Q4 = (tableau, i) => {
       return (tableau[i] == 'c') ? Q4(tableau, i + 1) : (tableau[i] == 'b') ? Q1(tableau, i + 1) : (tableau[i] == ' ') ? 1 : 0;
    }
    // etat Q5 (etat final)
-   // function Q5(tableau, i) {
-   //    if (tableau[i] == 'c') {
-   //       return Q5(tableau, i + 1);
-   //    }
-   //    if (tableau[i] == 'b') {
-   //       return Q1(tableau, i + 1);
-   //    }
-   //    //    s'il s'agit de dernier caractere de mot
-   //    if (tableau[i] == ' ') {
-   //       return 1;
-   //    }
-   //    return 0
-   // }
-   Q5 = (tableau, i) => {
+   
+   const Q5 = (tableau, i) => {
       return (tableau[i] == 'c') ? Q5(tableau, i + 1) : (tableau[i] == 'b') ? Q1(tableau, i + 1) : (tableau[i] == ' ') ? 1 : 0;
    }
-   verifier = (tableau) => {
+   const verifier = (tableau) => {
       return (tableau[0] == 'c') ? Q1(tableau, i + 1) :
          (tableau[0] == 'b') ? Q2(tableau, i + 1) : 0;
    }
